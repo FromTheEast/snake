7750e4ec9c6e1c8f730760fa8697f38c34bf8c89
lesyach <45921550+lesyach@users.noreply.github.com>
2020-03-11 17:23:06 +0300

    Lines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,13 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
+
+            VerticalLine line2 = new VerticalLine(10, 5, 10, '+');
+            line2.Drow();
+
+            
             Console.ReadLine();
         
         }
